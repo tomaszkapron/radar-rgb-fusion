f5d524a4edc89a4634e2250510f4d6dfb75787f2
tomaszkapron <tomasz.a.kapron@gmail.com>
2023-05-08 23:19:21 +0200

    general: exposing port for foxglove, update .gitignore

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 rocker --network host --privileged --nvidia --x11 --user --group-add plugdev --group-add sudo --name ros_container \
     --env "USER" \
+    --port 9090:9090 \
     --env "RMW_IMPLEMENTATION=rmw_cyclonedds_cpp" \
     --env "ROS_DOMAIN_ID=0" \
     --volume "${PWD}:${HOME}/${PWD##*/}" \
