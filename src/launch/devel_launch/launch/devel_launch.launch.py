64e57073e9df803fdb6756bf711a536ef6e86a24
tomaszkapron <tomasz.a.kapron@gmail.com>
2023-05-16 22:26:59 +0200

    feat: created launch file to start the stack

---
+++ src/launch/devel_launch/launch/devel_launch.launch.py
@@ -0,0 +1,83 @@
+# Copyright 2023 tomaszkapron
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction, ExecuteProcess
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def launch_setup(context, *args, **kwargs):
+    pkg_prefix = FindPackageShare("devel_launch")
+    config_param = PathJoinSubstitution([pkg_prefix, LaunchConfiguration('config_param_file')])
+    config_rviz = PathJoinSubstitution([pkg_prefix, 'rviz/devel.rviz'])
+
+    rviz2 = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', str(config_rviz.perform(context))],
+        condition=IfCondition(LaunchConfiguration('with_rviz'))
+    )
+     
+    static_transform_publisher = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='static_transform_publisher',
+        arguments=["1.85", "0.0", "0.3", "-1.57", "0.0", "0.0", "base_link", "radar"]
+    )
+
+    rosbag = ExecuteProcess(
+        cmd=[
+            "ros2",
+            "bag",
+            "play",
+            "--loop",
+            "/home/tomek/mgr_ws/rosbag2_2023_04_12-17_50_39",
+        ],
+        output="screen",
+    )
+    
+    return [
+        rviz2,
+        rosbag,
+        static_transform_publisher
+        ]
+
+
+def generate_launch_description():
+    declared_arguments = []
+
+    declared_arguments.append(
+            DeclareLaunchArgument(
+                'config_param_file',
+                default_value='param/defaults.param.yaml',
+                description='Node config (relative path).'
+            )
+    )
+
+    declared_arguments.append(
+            DeclareLaunchArgument(
+                'with_rviz',
+                default_value='True',
+                description='Run RViz2.'
+            )
+    )
+
+    return LaunchDescription([
+        *declared_arguments,
+        OpaqueFunction(function=launch_setup)
+    ])
